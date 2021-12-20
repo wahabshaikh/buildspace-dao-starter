6276d7634a6083f79d4ed29d29bfa601fed4d8a5
Wahab Shaikh <wahab.shaikh@somaiya.edu>
2021-12-20 09:28:19 +0530

    feat: connect wallet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 import "./index.css";
 import App from "./App.jsx";
 
-// Render the App component to the DOM
+// Support the Rinkeby chain
+const supportedChainIds = [4];
+
+// Support the Metamask wallet
+const connectors = {
+  injected: {},
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <App />
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
